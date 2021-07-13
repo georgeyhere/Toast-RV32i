339a1a20912e7567d5583bd493439b3e25dc47f8
George Yu <35508393+georgeyhere@users.noreply.github.com>
2021-07-12 17:09:49 -0700

    elf2hex

--- Test/riscv-tests_tb.sv
+++ Test/riscv-tests_tb.sv
@@ -18,9 +18,58 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+import   RV32I_definitions::*;
+import   testbench_pkg::*;
 
 
-module riscvTests_tb(
+module riscvTests_tb();
+  
+    reg Clk = 0;
+    reg Reset_n;
+    reg [31:0] mem_rd_data = 0;
+    wire [31:0] mem_addr;
+    wire [31:0] mem_wr_data;
+    wire        mem_wr_en;
+    wire        mem_rst; 
+    
+    bit [31:0] instruction;
+    bit [31:0] instruction1;
 
+    reg [31:0] pc;
+    
+    reg [3:0] checker_cycles;
+    reg [4:0] checker_rd1, checker_rd2;
+    
+    
+    wire [31:0] regfile_rd1 = UUT.ID_inst.RV32I_REGFILE.Regfile_data[checker_rd1]; 
+    wire [31:0] regfile_rd2 = UUT.ID_inst.RV32I_REGFILE.Regfile_data[checker_rd2]; 
+
+    int unsigned m, k;
+    int unsigned expected;
+
+    int passed_cnt;
+    int failed_cnt;
+
+    ToastCore UUT(
+    .Clk         (Clk),
+    .Reset_n     (Reset_n),
+    .mem_rd_data (mem_rd_data),
+    .mem_addr    (mem_addr),
+    .mem_wr_en   (mem_wr_en),
+    .mem_rst     (mem_rst)
     );
+    
+    always#(10) Clk = ~Clk;     
+    
+    initial begin
+        $dumpfile("add.vcd");
+        $dumpvars(2, UUT);
+    end
+    
+    initial begin
+        Reset_n = 0;
+        #100;
+        Reset_n = 1;
+    end
+    
 endmodule
